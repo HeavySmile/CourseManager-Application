dd31184c64679a52fc1fdb6f8f937602e5852a87
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-16 19:36:49 +0300

    Add project files.

---
+++ CourseManager/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using CourseManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CourseManager
+{
+    class StartUp : BootstrapperBase
+    {
+        public StartUp()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<MainViewModel>();
+
+        }
+    }
+}
